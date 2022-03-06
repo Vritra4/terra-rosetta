6418ae7d4e0ec06c5233c9e1fd766f613adba560
Seunghwa Lee <Vritra@hitel.net>
2022-03-06 22:33:21 +0900

    add simple script

---
+++ run_mainnet_with_data.sh
@@ -0,0 +1,1 @@
+docker run -d -p 1317:1317 -p 26657:26657 -p 26656:26656 -p 8080:8080 -e MAX_OUTBOUND=40 -v "$(pwd)"/data:/root/.terra/data  --name terra-mainnet-pruned terramoney/rosetta-mainnet:0.1
